0143d0b18079b02e884d3b3a5bf21048d320ef5c
SoneJojo <gjoao1821@gmail.com>
2025-11-18 22:45:49 -0300

    Migrations merge

---
+++ IFSPStore.Repository/Migrations/20251014140703_CategoryAddDescription.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IFSPStore.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class CategoryAddDescription : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SemMapeamento",
+                table: "Category");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Category",
+                type: "varchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Category",
+                type: "varchar(1000)",
+                maxLength: 1000,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Category");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Category",
+                type: "varchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SemMapeamento",
+                table: "Category",
+                type: "longtext",
+                nullable: false);
+        }
+    }
+}
